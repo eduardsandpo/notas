0286bcd3702b46bf46e8d5732dfa01d1154a3776
Jose Eduardo Sandoval Polanco <eduardsand.po@gmail.com>
2023-05-21 11:04:12 -0700

    setup

---
+++ common/js/notes_filler.js
@@ -0,0 +1,1 @@
+console.log("common/js/notes_filler.js -> TODO: Add code to fill in notes list from storage or db.");
---
+++ common/js/todos_filler.js
@@ -0,0 +1,1 @@
+console.log("common/js/todos_filler.js -> TODO: Add code to fill in todos from storage or db.");
